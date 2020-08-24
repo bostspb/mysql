93a2b94a5b4984b7468cf3a6793bc0005fcdbc81
Zakhar <z.chernyak@filanco.ru>
2020-08-24 14:36:08 +0300

    опечатка

--- Scripts/lesson02/task03.sql
+++ Scripts/lesson02/task03.sql
@@ -3,11 +3,8 @@
  * разверните содержимое дампа в новую базу данных sample.
  */
 
-DROP DATABASE IF EXISTS example;
-CREATE DATABASE example DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
+DROP DATABASE IF EXISTS sample;
+CREATE DATABASE sample DEFAULT CHARACTER SET utf8 DEFAULT COLLATE utf8_general_ci;
 
 -- 1) Через интерфейс DBeaver делаю Dump database 'example'.
 -- 2) Также через интерфейс DBeaver делаю Restore database в базу 'sample'.
--- 3) Все оказалось слишком просто - идем в консоль и пробуем это же самое сделать через mysqldump, 
--- (кстати DBeaver тоже использовал именно эту утилиту, когда делал дамп).
--- 
