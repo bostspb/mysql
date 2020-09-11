5c9cfd17f7b97ebaa3608644e0146b27dd363dda
Zakhar <z.chernyak@filanco.ru>
2020-09-11 07:54:17 +0300

    докоммит

--- Scripts/lesson07/task02.sql
+++ Scripts/lesson07/task02.sql
@@ -6,5 +6,15 @@
 select p.id, p.name, c.name 
 from products p
 left join catalogs c on c.id = p.catalog_id;
-
+/*
+id|name                   |name             |
+--|-----------------------|-----------------|
+ 1|Intel Core i3-8100     |Процессоры       |
+ 2|Intel Core i5-7400     |Процессоры       |
+ 3|AMD FX-8320E           |Процессоры       |
+ 4|AMD FX-8320            |Процессоры       |
+ 5|ASUS ROG MAXIMUS X HERO|Материнские платы|
+ 6|Gigabyte H310M S2H     |Материнские платы|
+ 7|MSI B250M GAMING PRO   |Материнские платы|
+ */
 
