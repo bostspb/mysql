3e12e001b10265785f4a819bcea0d36f737c2852
Zakhar <z.chernyak@filanco.ru>
2020-09-27 09:33:08 +0300

    Урок 11. Видеоурок. Оптимизация запросов. NoSQL

--- Scripts/lesson11/task09_01.sql
+++ Scripts/lesson11/task09_01.sql
@@ -8,3 +8,40 @@
  *
  */
 
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs (  
+  created_at datetime DEFAULT CURRENT_TIMESTAMP,
+  table_name varchar(255) NOT NULL,
+  primary_key bigint(20) unsigned,
+  name_value varchar(255) DEFAULT NULL
+) ENGINE=Archive DEFAULT CHARSET=utf8mb4;
+
+
+DELIMITER //
+DROP TRIGGER IF EXISTS users_log//
+CREATE TRIGGER users_log AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (table_name, primary_key, name_value) VALUES ('users', NEW.id, NEW.name);
+END//
+DELIMITER ;
+
+
+DELIMITER //
+DROP TRIGGER IF EXISTS catalogs_log//
+CREATE TRIGGER catalogs_log AFTER INSERT ON catalogs
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (table_name, primary_key, name_value) VALUES ('catalogs', NEW.id, NEW.name);
+END//
+DELIMITER ;
+
+
+DELIMITER //
+DROP TRIGGER IF EXISTS products_log//
+CREATE TRIGGER products_log AFTER INSERT ON products
+FOR EACH ROW
+BEGIN
+	INSERT INTO logs (table_name, primary_key, name_value) VALUES ('products', NEW.id, NEW.name);
+END//
+DELIMITER ;
--- Scripts/lesson11/task10_03.sql
+++ Scripts/lesson11/task10_03.sql
@@ -4,14 +4,89 @@
  *	3. Организуйте хранение категорий и товарных позиций учебной базы данных shop в СУБД MongoDB.
  *
  */
+  
 
+--
+-- Подготовка
+--
 
-/*
+Устанавливаем MongoDB
 docker pull mongo
+docker run -d -p 127.0.0.1:27017:27017 --name mongo_gb mongo
 
-docker run --name mongo_gb -d mongo
+Устанавливаем простой графический клиент Robo 3T
+https://robomongo.org/
 
-заходим в консоль контейнера и работаем через консольного клиента
-mongo
+Или можно работать в консольном клиенте `mongo` в консоли контейнера
 
-*/
+
+--
+-- Решение задачи
+--
+
+Создаем БД
+use shop
+
+
+Добавляем товары
+db.products.insertMany([
+	{name: 'Intel Core i3-8100', description: 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', price: 7890.00, catalog_id: 1},
+	{name: 'Intel Core i5-7400', description: 'Процессор для настольных персональных компьютеров, основанных на платформе Intel.', price: 12700.00, catalog_id: 1},
+	{name: 'AMD FX-8320E', description: 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', price: 4780.00, catalog_id: 1},
+	{name: 'AMD FX-8320', description: 'Процессор для настольных персональных компьютеров, основанных на платформе AMD.', price: 7120.00, catalog_id: 1},
+	{name: 'ASUS ROG MAXIMUS X HERO', description: 'Материнская плата ASUS ROG MAXIMUS X HERO, Z370, Socket 1151-V2, DDR4, ATX', price: 19310.00, catalog_id: 2},
+	{name: 'Gigabyte H310M S2H', description: 'Материнская плата Gigabyte H310M S2H, H310, Socket 1151-V2, DDR4, mATX', price: 4790.00, catalog_id: 2},
+	{name: 'MSI B250M GAMING PRO', description: 'Материнская плата MSI B250M GAMING PRO, B250, Socket 1151, DDR4, mATX', price: 5060.00, catalog_id: 2}
+])
+
+
+Добавляем категории
+db.catalogs.insertMany([
+	{_id: 1, name: 'Процессоры'},
+	{_id: 2, name: 'Материнские платы'},
+	{_id: 3, name: 'Видеокарты'},
+	{_id: 4, name: 'Жесткие диски'},
+	{_id: 5, name: 'Оперативная память'}
+])
+
+
+
+Проверяем - запрос на выдачу всех товаров категории 'Процессоры', у которой id = 1
+db.products.find({ 'catalog_id': 1 })
+
+Результат:
+/* 1 */
+{
+    "_id" : ObjectId("5f701e8cad68112178fdbadf"),
+    "name" : "Intel Core i3-8100",
+    "description" : "Процессор для настольных персональных компьютеров, основанных на платформе Intel.",
+    "price" : 7890.0,
+    "catalog_id" : 1.0
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("5f701e8cad68112178fdbae0"),
+    "name" : "Intel Core i5-7400",
+    "description" : "Процессор для настольных персональных компьютеров, основанных на платформе Intel.",
+    "price" : 12700.0,
+    "catalog_id" : 1.0
+}
+
+/* 3 */
+{
+    "_id" : ObjectId("5f701e8cad68112178fdbae1"),
+    "name" : "AMD FX-8320E",
+    "description" : "Процессор для настольных персональных компьютеров, основанных на платформе AMD.",
+    "price" : 4780.0,
+    "catalog_id" : 1.0
+}
+
+/* 4 */
+{
+    "_id" : ObjectId("5f701e8cad68112178fdbae2"),
+    "name" : "AMD FX-8320",
+    "description" : "Процессор для настольных персональных компьютеров, основанных на платформе AMD.",
+    "price" : 7120.0,
+    "catalog_id" : 1.0
+}
